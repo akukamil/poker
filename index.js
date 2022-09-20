a573766aa8edff4e840492c0758b989f06bd8e01
akukamil <chamilioner_kid@mail.ru>
2022-09-20 09:09:51 +0300

    123

--- index.js
+++ index.js
@@ -2958,12 +2958,14 @@ main_menu= {
 
 	chips_button_down : async function() {
 		
-		if (my_data.rating > 50) {
-			sound.play('locked');
+		if (my_data.rating < 50) {
+			
+			let res = await confirm_dialog.show(['Получить фишки можно только если у вас их меньше 50. Хотите все равно посмотреть рекламу?','You can only get chips if you have less than 50 of them. Do you want to watch the ad anyway?'][LANG]);
+			if (res = 'ok')	await ad.show2();
 			return;
 		}
 		
-		let res = await confirm_dialog.show(['Получить 100 фишек за просмотр рекламы?','Get 100 chips (reward video)'][LANG]);
+		let res = await confirm_dialog.show(['Получить 100 фишек за просмотр рекламы?','Get 100 chips (reward video)?'][LANG]);
 		if (res = 'ok') {
 			
 			let res2 = await ad.show2();
@@ -4433,7 +4435,6 @@ async function init_game_env(lang) {
 	//отображаем шкалу загрузки
 	document.body.innerHTML='<style>html,body {margin: 0;padding: 0;height: 100%;	}body {display: flex;align-items: center;justify-content: center;background-color: rgba(41,41,41,1);flex-direction: column	}#m_progress {	  background: #1a1a1a;	  justify-content: flex-start;	  border-radius: 5px;	  align-items: center;	  position: relative;	  padding: 0 5px;	  display: none;	  height: 50px;	  width: 70%;	}	#m_bar {	  box-shadow: 0 1px 0 rgba(255, 255, 255, .5) inset;	  border-radius: 5px;	  background: rgb(119, 119, 119);	  height: 70%;	  width: 0%;	}	</style></div><div id="m_progress">  <div id="m_bar"></div></div>';
 
-		
 		
 	//устанаваем язык
 	LANG = lang;
