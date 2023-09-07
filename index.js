269f896b0a1cc3ca0ad7f6573e55225fc6449b4b
akukamil <chamilioner_kid@mail.ru>
2023-09-07 16:55:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -64,11 +64,11 @@ class chat_record_class extends PIXI.Container {
 		super();	
 		this.resolver=0;
 		this.text=new PIXI.BitmapText('***', {fontName: 'mfont',fontSize:25,lineSpacing:39}); 
-		this.text.tint=0xFFFF00;
+		this.text.tint=0x55bbdd;
 		this.text.maxWidth=290;
 		
 		this.name_text=new PIXI.BitmapText('***', {fontName: 'mfont',fontSize: 25}); 
-		this.name_text.tint=0xFFFFFF;
+		this.name_text.tint=0xbbff00;
 		
 		this.visible=false;
 		this.addChild(this.text,this.name_text)
