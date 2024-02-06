863de3db2a7ddfc502c4801d552ab66f9417daa0
akukamil <chamilioner_kid@mail.ru>
2024-02-06 17:45:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3973,7 +3973,7 @@ async function init_game_env(env) {
 	my_data.pic_url=other_data?.pic_url || my_data.orig_pic_url;
 	
 	//загружаем мои данные в кэш
-	await players_cache.update(my_data.uid,{pic_url:my_data.pic_url,name:my_data.name,country:my_data.country});
+	await players_cache.update(my_data.uid,{pic_url:my_data.pic_url,name:my_data.name,country:my_data.country||0});
 	await players_cache.update_avatar(my_data.uid);
 
 
