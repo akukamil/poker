e1af2eaa9316b2e950788270fdeaa9cd38818c83
akukamil <chamilioner_kid@mail.ru>
2024-06-17 08:04:33 +0300

    123

--- index.js
+++ index.js
@@ -3115,6 +3115,10 @@ tables_menu={
 			tables_menu.table_data_updated(objects.t_table3_players_num,data.val(),0)
 		})
 
+		objects.t_table1_ante.text='Анте '+ante_data['table1'];
+		objects.t_table2_ante.text='Анте '+ante_data['table2'];
+		objects.t_table3_ante.text='Анте '+ante_data['table3'];
+
 		objects.table_menu_info.text=''
 		this.init_payments();
 		
