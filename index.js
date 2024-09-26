7fb70791009a5935d6fc1eacadca18a2073c957a
akukamil <chamilioner_kid@mail.ru>
2024-09-26 09:00:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4403,7 +4403,7 @@ dr={
 		
 		fbs.ref('players/'+my_data.uid+'/PRV/DR/claimed').set(this.claimed).then(()=>{	
 			game.change_my_balance(card.reward);
-			game.change_stickers_num(10);
+			stickers.change_stickers_num(10);
 			tables_menu.update_my_data();
 		})			
 		
